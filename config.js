0008a383969365aef8f8f661660f26300eaa9eee
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2016-04-19 21:13:40 -0300

    Add nedb adapter
    To change the path of database set DATABASE_NEDB_PATH env variable
    
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
+var path = require('path');
+
 module.exports = {
   httpServer: {
     port: process.env.HTTP_SERVER_PORT || 80
   },
 
   database: {
-    adapter: process.env.DATABASE_ADAPTER || 'nedb'
+    adapter: process.env.DATABASE_ADAPTER || 'nedb',
+
+    nedb: {
+      path: process.env.DATABASE_NEDB_PATH || path.join(__dirname)
+    }
   },
 
   queryParser: {
