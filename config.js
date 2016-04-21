15aa86cd8a9b89d505c46a850b1bb88266eb0e5b
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2016-04-20 23:04:28 -0300

    Remove database adapters
    The database stuff is not necessary anymore because all database
    operations has been done in the backend
    
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

--- config.js
+++ config.js
@@ -5,14 +5,6 @@ module.exports = {
     port: process.env.HTTP_SERVER_PORT || 80
   },
 
-  database: {
-    adapter: process.env.DATABASE_ADAPTER || 'nedb',
-
-    nedb: {
-      path: process.env.DATABASE_NEDB_PATH || path.join(__dirname)
-    }
-  },
-
   rest: {
     url: process.env.REST_URL,
     apiKey: process.env.REST_API_KEY || ""
