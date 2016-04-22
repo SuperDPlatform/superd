6d96f59a2864cde627f6b50b650b51682928cc75
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2016-04-22 19:49:59 -0300

    Enable multiple nodes through redis
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

--- config.js
+++ config.js
@@ -5,6 +5,11 @@ module.exports = {
     port: process.env.HTTP_SERVER_PORT || 80
   },
 
+  redis: {
+    host: process.env.REDIS_HOST || "localhost",
+    port: process.env.REDIS_PORT || 6379
+  },
+
   rest: {
     url: process.env.REST_URL,
     apiKey: process.env.REST_API_KEY || ""
